68573e303358d9192d23814cee9a6023dc21d7b4
ihab abadi <ihab@utopios.net>
2024-07-01 18:09:00 +0200

    avec les ressources pour le TP

---
+++ correction-tp1-slide-19/net-app.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MESSAGE "Hello from net-app!\n"
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept failed");
+            continue;
+        }
+        send(new_socket, MESSAGE, strlen(MESSAGE), 0);
+        close(new_socket);
+    }
+
+    return 0;
+}
---
+++ etapes-tp1.md
@@ -0,0 +1,19 @@
+1. ## Préparation des fichiers sources
+- net-app.c
+- Makefile
+2. ## Créer un ZIP avec l'application
+```
+```
+
+3. ## Création de la structure de custom layer
+```
+```
+
+4. ## L'ajout de la recette de l'application
+
+- Créer la structure de la recette
+- Créer le fichier .bb
+
+5. ## L'ajout du custom Layer au build
+
+6. ## Construire l'image
